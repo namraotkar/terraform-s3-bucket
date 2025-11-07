9b5309a6ffa6585b24662367d36dd745cc1bd851
namraotkar <nikhil.amraotkar@gmail.com>
2025-11-07 14:04:10 -0600

    Create bonus.py

---
+++ bonus.py
@@ -0,0 +1,10 @@
+# bonus.py
+# Prints your name multiple times
+
+def print_name(name, times):
+    for i in range(times):
+        print(name)
+
+if __name__ == "__main__":
+    # Change times if you want more repetitions
+    print_name("Nikhil Amraotkar", 10)
