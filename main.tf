8d17950e9785da62d26b3cd14065815ece9f60dd
Nikhil Amraotkar <nikhil.amraotkar@gmail.com>
2025-11-07 15:12:40 +0000

    Initial Terraform module for S3

---
+++ main.tf
@@ -0,0 +1,8 @@
+" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  acl    = "private"
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+tring
+  default = "us-east-2"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket"
+}
