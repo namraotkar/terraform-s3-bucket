69ac80dd0e6381ba98e5c3354af1fa692352cf40
namraotkar <nikhil.amraotkar@gmail.com>
2025-11-08 10:53:40 -0600

    Update main.tf

--- ec2-grain/main.tf
+++ ec2-grain/main.tf
@@ -2,19 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "TorqueEC2"
-  }
-}
-
-output "instance_id" {
-  value = aws_instance.example.id
-}
-
-output "public_ip" {
-  value = aws_instance.example.public_ip
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  acl    = "private"
 }
